15a4ad73403b6624bb710a459df798ce9db2ed01
ranichandnirani <chandnirani229@gmail.com>
2026-01-05 13:27:13 +0530

    Installing module and printing some random jokesfrom pyjokes

---
+++ Chapter-01/module.py
@@ -0,0 +1,10 @@
+# pyjoke module . we install it (pip install pyjoke) it is a single line comment
+import pyjokes
+
+joke = pyjokes.get_joke()
+print(joke)
+
+"""
+thanks ,
+that was my program(multi line comment)
+"""
