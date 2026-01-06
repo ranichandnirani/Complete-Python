5182589c6faf21a561b51dcf2a48cf39ab978eda
ranichandnirani <chandnirani229@gmail.com>
2026-01-06 17:07:20 +0530

    Operators and typecasting

---
+++ Chapter-02/operators.py
@@ -0,0 +1,38 @@
+# Arithmetic operators (+, -, *, /, %)
+
+a = 6
+b = 4
+print(a + b)
+
+# Assignment operators(=, +=, -=, *=, /=, %=)
+x = 8 - 5 #assign 2-5 in a
+print(x)
+
+b = 6
+b += 4 # increment the value of b by 4 and then assign it to b
+b -= 4 # decrement the value of b by 4 and then assign it to b
+
+print(b)
+
+#comparison operator(>, >=, <, <=, ==, !=)
+
+d = 5 < 7
+
+print(d)
+
+# logical operators(&&, ||, !)
+
+e = True or False
+# truth table or
+print("True or False is= ", True or False)
+print("True or True is= ", True or True)
+print("False or True is= ", False or True)
+print ("False or False is= ", False or False)
+
+# Truth table of and
+print("True or False is= ", True  and False)
+print("True or True is= ", True and True)
+print("False or True is= ", False and True)
+print ("False or False is= ", False and False)
+
+print(not(True))
