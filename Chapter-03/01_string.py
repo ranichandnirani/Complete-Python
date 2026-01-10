05117678341ba54c86358938a0c0011d6f8e5465
ranichandnirani <chandnirani229@gmail.com>
2026-01-10 11:52:18 +0530

    Chapter 3 strings

---
+++ Chapter-03/01_string.py
@@ -0,0 +1,20 @@
+name = "chandni"
+# positive slicing
+
+nameshort = name[0:3] #Star from index 0 all the way till 3 (excluding 3)
+print(nameshort)
+character1 = name[1]
+print(character1)
+
+# negative slicing
+
+name2 = "chandni"
+
+print(name[0:3])
+
+print(name[-4:-1])
+print(name[1:4])
+
+print(name[:4]) #same as print(name[0:4])
+print(name[1:]) #same as print(name[1:7])
+print(name[1:7]) 
