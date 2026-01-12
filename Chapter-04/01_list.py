86dc83f9fed394df8909823ec3729c8e66ffc89d
ranichandnirani <chandnirani229@gmail.com>
2026-01-12 17:48:18 +0530

    List & Tuples

---
+++ Chapter-04/01_list.py
@@ -0,0 +1,7 @@
+friends = ["Apple", "Orange", 5, 234.76, False, "Chandni", "Gaurav"]
+
+print(friends[0])
+friends[0] = "Grapes" # unlike Strings lists are mutable
+
+print(friends[0])
+print(friends[1:4])
