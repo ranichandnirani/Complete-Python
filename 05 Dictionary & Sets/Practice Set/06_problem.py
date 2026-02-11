93fb1650db3914acb53d416be6634af9cb3c3f6f
ranichandnirani <chandnirani229@gmail.com>
2026-02-11 17:31:48 +0530

    Practice set done

---
+++ 05 Dictionary & Sets/Practice Set/06_problem.py
@@ -0,0 +1,22 @@
+# 6. Create an empty dictionary. Allow 4 friends to enter their favorite language as
+# value and use key as their names. Assume that the names are unique.
+
+d = {}
+
+name = input("Enter friend name: ")
+lang = input("Enter language: ")
+d.update({name: lang})
+
+name = input("Enter friend name: ")
+lang = input("Enter language: ")
+d.update({name: lang})
+
+name = input("Enter friend name: ")
+lang = input("Enter language: ")
+d.update({name: lang})
+
+name = input("Enter friend name: ")
+lang = input("Enter language: ")
+d.update({name: lang})
+
+print(d)
