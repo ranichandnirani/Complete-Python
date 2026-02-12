34c85417444823d2680f86ed2dc83220063c005a
ranichandnirani <chandnirani229@gmail.com>
2026-02-12 18:38:14 +0530

    Start conditional expressions

---
+++ 06 Conditional Expression/01_conditionals.py
@@ -0,0 +1,9 @@
+a = int(input("enter the age: "))
+
+if(a >= 18):
+    print("You are above the age of consent")
+    print("Good for you")
+else:
+    print("You aare above the age of concent")    
+
+print("End of program!")    
