f60cb1208907e52dfa4e1dacd7cff44b11154fd0
ranichandnirani <chandnirani229@gmail.com>
2026-02-14 17:33:34 +0530

    if elif else ladder statement

--- 06 Conditional Expression/01_conditionals.py
+++ 06 Conditional Expression/01_conditionals.py
@@ -1,5 +1,7 @@
 a = int(input("enter the age: "))
 
+# if else statement
+
 if(a >= 18):
     print("You are above the age of consent")
     print("Good for you")
---
+++ 06 Conditional Expression/02_if_elif_else_ladder.py
@@ -0,0 +1,18 @@
+a = int(input("enter the age: "))
+
+# if elif else ladder statement
+
+if(a >= 18):
+    print("You are above the age of consent")
+    print("Good for you")
+
+elif(a < 0):
+    print("You are entring invalid age.")
+
+elif(a == 0):
+    print("You are entering 0 which is not valid age")  
+
+else:
+    print("You are above the age of concent")    
+
+print("End of program!")    
